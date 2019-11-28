bf100f19eb3398cfbe1d3f8134bf50dd870f7021
Amanda MacInnis <amanda.macinnis@stonybrook.edu>
2019-11-27 19:09:46 -0500

    fixed error in data reduction steps, updated source extractor config to use same aperture for each image, and other more minor updates

--- calibrate_magnitudes.py
+++ calibrate_magnitudes.py
@@ -31,43 +31,78 @@ cat_fnames = os.listdir(os.path.join(os.getcwd(), 'source_extractor_output'))
 
 cat_files = {'B': [], 'V': []}
 for fname in cat_fnames:
-	if 'B' in fname:
+	if ('B' in fname):# and ('large' in fname):
 		cat_files['B'].append(fname)
-	elif 'V' in fname:
+	elif ('V' in fname):# and ('large' in fname):
 		cat_files['V'].append(fname)
+
 
 # note the magnitudes are found from the https://www.aavso.org/ finding charts
 # create a dict of reference stars to use for calibration
 ref_stars = {}
-ref_stars['merope'] = {'ra': get_ra(3., 46., 19.5738), 
-	'dec': get_dec(23., 56., 54.0812), 
-	'mag_V': 4.180, 'mag_B': 4.120}       # website lists source as 'BSC' 
+ref_stars['merope'] = {'ra': get_ra(3., 46., 19.55), 
+	'dec': get_dec(23., 56., 53.3), 
+	'mag_V': 4.180, 'mag_V_err': 0.100, 'mag_B': 4.120, 'mag_B_err': 0.141,   # website lists source as 'BSC'
+	'cat_files': [('merope', 0.1, False), ('merope', 1, True), 
+		('merope', 10, True), ('hd23511', 1, True)]}     # list of which catalogs to use, and whether to use large aperture
+
 ref_stars['atlas'] = {'ra': get_ra(3., 49., 9.57),
 	'dec': get_dec(24., 3., 12.3),
-	'mag_V': 3.630, 'mag_B': 3.540} # BSC
+	'mag_V': 3.630, 'mag_V_err': 0.100, 'mag_B': 3.540, 'mag_B_err': 0.141, # BSC
+	'cat_files': [('atlas', 0.1, False), ('atlas', 1, True)]} 
+'''
 ref_stars['hd23778'] = {'ra': get_ra(3., 48., 34.78),
 	'dec': get_dec(24., 10., 53.1),
-	'mag_V': 9.063, 'mag_B': 9.519} # B from Tycho-2, V from ASAS
+	'mag_V': 9.063, 'mag_V_err': 0.015, 'mag_B': 9.519, 'mag_B_err': 0.046,# B from Tycho-2, V from ASAS
+	'cat_files': [('hd23778', 1, False), ('atlas', 1, False), ('atlas', 5, False)]}
+'''
 ref_stars['hd23778_ref1'] = {'ra': get_ra(3., 48., 30.07),
 	'dec': get_dec(24, 20., 44.5),
-	'mag_V': 6.964, 'mag_B': 7.023} # WBVR 
+	'mag_V': 6.964, 'mag_V_err': 0.100, 'mag_B': 7.023, 'mag_B_err': 0.141, # WBVR
+	'cat_files': [('hd23778', 1, False)]} 
+
 ref_stars['hd23778_ref2'] = {'ra': get_ra(3., 49., 25.98),
 	'dec': get_dec(24., 14., 52.5),
-	'mag_V': 7.993, 'mag_B': 8.095} # WBVR
-ref_stars['hd23511_ref1'] = {'ra': get_ra(3., 46., 19.55),
-	'dec': get_dec(23., 56., 53.3),
-	'mag_V': 4.180, 'mag_B': 4.120} # BSC
+	'mag_V': 7.993, 'mag_V_err': 0.100, 'mag_B': 8.095, 'mag_B_err': 0.141, # WBVR
+	'cat_files': [('hd23778', 1, False), ('atlas', 1, False), ('atlas', 5, False)]}
+'''
 ref_stars['hd23511_ref2'] = {'ra': get_ra(3., 46., 34.26),
 	'dec': get_dec(24., 8., 17.3),
-	'mag_V': 11.745, 'mag_B': 12.438} # APASS
+	'mag_V': 11.745, 'mag_V_err': 0.050, 'mag_B': 12.438, 'mag_B_err': 0.082, # APASS
+	'cat_files': [('hd23479', 5, False), ('hd23479', 15, False), ('hd23511', 10, False)]}
+
 ref_stars['celaeno_ref1'] = {'ra': get_ra(3., 45., 12.51),
 	'dec': get_dec(24., 28., 1.9),
-	'mag_V': 4.3, 'mag_B': 4.19} # BSC
-#ref_stars['hd23479_ref1'] = {'ra': get_ra(3., 45., 49.57),
-#	'dec': get_dec(24., 22., 3.4),
-#	'mag_V': 3.870, 'mag_B': 3.800} # BSC
+	'mag_V': 4.3, 'mag_V_err': 0.100, 'mag_B': 4.190, 'mag_B_err': 0.141, # BSC
+	'cat_files': [('celaeno', 1, True), ('celaeno', 10, True)]}
+
+ref_stars['hd23479_ref1'] = {'ra': get_ra(3., 45., 49.57),
+	'dec': get_dec(24., 22., 3.4),
+	'mag_V': 3.870, 'mag_V_err': 0.100, 'mag_B': 3.800, 'mag_B_err': 0.141, # BSC
+	'cat_files': [('hd23479', 1, True), ('hd23479', 5, True)]}
+'''
 
 
+# this is horrible, but it gets the job done ... for each star, get only the catalog filenames we want to use
+for star in ref_stars.keys():
+	ref_stars[star]['fnames'] = {}
+	for filt in filters:
+		#print "\n", star, filt
+		ref_stars[star]['fnames'][filt] = [] # list of catalog filenames for each star in each filter
+		for cat_info in ref_stars[star]['cat_files']:
+			star_name, exp_time, use_large_ap = cat_info # get exp time and aperture for catalog
+			exp_str = '_{}s_'.format(exp_time)
+			#print cat_info, exp_time, exp_str, use_large_ap
+			for fname in cat_files[filt]:
+				if (star_name in fname) and (exp_str in fname):
+					#print fname
+					if use_large_ap and ('large' in fname):
+						ref_stars[star]['fnames'][filt].append(fname)
+					elif (not use_large_ap) and ('large' not in fname):
+						ref_stars[star]['fnames'][filt].append(fname)
+		#print "files:"
+		#for fname in ref_stars[star]['fnames'][filt]:
+		#	print fname
 
 
 # look at the measured magnitude of each
@@ -78,7 +113,7 @@ for star in ref_stars.keys():
 	for filt in filters:
 		measured_mags = []
 		measured_mag_errs = []
-		for fname in cat_files[filt]:
+		for fname in ref_stars[star]['fnames'][filt]:#cat_files[filt]:
 			# load the catalog
 			ras, decs, fluxs, flux_errs, mags, mag_errs, background, max_flux = np.loadtxt('source_extractor_output/'+fname, unpack=True)
 			# get a list of angular distances (in arcmin) between where we think the star is, and the objects detected by source extractor
@@ -89,7 +124,7 @@ for star in ref_stars.keys():
 				loc = np.where(distances == np.min(distances))
 
 				# add the values of the magnitude and its error to a list if the magnitude is detected and the star isn't saturated
-				if (max_flux[loc]+background[loc] <= 50000.) and (mags[loc] < 99.) and (mag_errs[loc] < 99.):
+				if (max_flux[loc]+background[loc] <= 60000.) and (mags[loc] < 99.) and (mag_errs[loc] < 99.):
 					measured_mags.append(mags[loc])
 					measured_mag_errs.append(mag_errs[loc])
 		# take the average of the measurements
@@ -102,7 +137,8 @@ for star in ref_stars.keys():
 			ref_stars[star]['measured_mag_{}'.format(filt)] = mag
 			ref_stars[star]['measured_mag_{}_err'.format(filt)] = mag_err
 			ref_stars[star]['mag_{}_offset'.format(filt)] = ref_stars[star]['mag_{}'.format(filt)] - mag
-			print "{} {} known mag = {}, measured mag = {:1.3f} +/- {:1.3f}, diff = {:1.3f}, standard dev = {:1.3f}.".format(star, filt, ref_stars[star]['mag_{}'.format(filt)], mag, mag_err, ref_stars[star]['mag_{}'.format(filt)] - mag, np.std(measured_mags))
+			ref_stars[star]['mag_{}_offset_err'.format(filt)] = np.sqrt(mag_err**2 + ref_stars[star]['mag_{}_err'.format(filt)]**2)
+			print "{} {} known mag = {} +/- {}, measured mag = {:1.3f} +/- {:1.3f}, diff = {:1.3f} +/- {:1.4f}, standard dev of measurements = {:1.3f}, npts = {}.".format(star, filt, ref_stars[star]['mag_{}'.format(filt)], ref_stars[star]['mag_{}_err'.format(filt)], mag, mag_err, ref_stars[star]['mag_{}_offset'.format(filt)], ref_stars[star]['mag_{}_offset_err'.format(filt)], np.std(measured_mags), len(measured_mags))
 
 # calculate the zero offset
 mag_diffs = {'V': {'diff': [], 'err': []}, 'B': {'diff': [], 'err': []}}
@@ -110,13 +146,25 @@ for star in ref_stars.keys():
 	for filt in filters:
 		if 'mag_{}_offset'.format(filt) in ref_stars[star].keys():
 			mag_diffs[filt]['diff'].append(ref_stars[star]['mag_{}_offset'.format(filt)])
-			mag_diffs[filt]['err'].append(ref_stars[star]['measured_mag_{}_err'.format(filt)])
+			mag_diffs[filt]['err'].append(ref_stars[star]['mag_{}_offset_err'.format(filt)])
 
 # take the average for each filter
+print "\n"
 mag_offset = {}
 for filt in filters:
 	wts = 1. / np.array(mag_diffs[filt]['err'])**2
 	mag_offset[filt] = {}
 	mag_offset[filt]['offset'] = np.average(np.array(mag_diffs[filt]['diff']), weights=wts)
 	mag_offset[filt]['err'] = np.sqrt(1. / np.sum(wts))
-	print "{}: offset = {} +/- {}".format(filt, mag_offset[filt]['offset'], mag_offset[filt]['err']) 
+	print "{}: offset = {:1.3f} +/- {:1.3f}".format(filt, mag_offset[filt]['offset'], mag_offset[filt]['err']) 
+
+for star in ref_stars.keys():
+	print "\n",star
+	star_info = ref_stars[star]
+	print "known mags: B = {} +/- {}, V = {} +/- {}".format(star_info['mag_B'], star_info['mag_B_err'], star_info['mag_V'], star_info['mag_V_err'])
+	B = ref_stars[star]['measured_mag_{}'.format('B')] + mag_offset['B']['offset']
+	Berr = np.sqrt(ref_stars[star]['measured_mag_{}_err'.format('B')]**2 + star_info['mag_B_err']**2)
+	V = ref_stars[star]['measured_mag_{}'.format('V')] + mag_offset['V']['offset']
+	Verr = np.sqrt(ref_stars[star]['measured_mag_{}_err'.format('V')]**2 + star_info['mag_V_err']**2)
+	print "corrected mag: B = {:1.3f} +/- {:1.3f}, V = {:1.3f} +/- {:1.3f}".format(B,Berr,V,Verr)
+	print "fractional difference: B: {:1.3f}, V: {:1.3f}".format((B - star_info['mag_B'])/star_info['mag_B'], (V - star_info['mag_V'])/star_info['mag_V'])
--- get_catalog.py
+++ get_catalog.py
@@ -33,17 +33,26 @@ def get_distances(ra, dec, coords):
 		distances.append(get_angular_dist(ra, dec, coord[0], coord[1])[1])
 	return np.array(distances)
 
+'''
+B: offset = 19.061 +/- 0.071
+V: offset = 19.882 +/- 0.050
+'''
 
 # offset found from using the known magnitudes of ref stars
-offset = {'B': 19.6759, 'V': 20.2690}
-offset_err = {'B': 0.0001, 'V': 0.0002}
+#offset = {'B': 19.6701, 'V': 20.4728}
+#offset_err = {'B': 0.0001, 'V': 0.0002}
+#offset = {'B': 20.648, 'V': 21.219}
+#offset_err = {'B': 0.034, 'V': 0.014}
+offset = {'B': 19.061, 'V': 19.882}
+offset_err = {'B': 0.071, 'V': 0.050}
 # extinction found using NASA extragalactic database
 ext = {'B': 0.863, 'V': 0.653}
 
-fmax = 50000. # max counts per pixel
+fmax = 60000. # max counts per pixel
 
 filters  = ['B', 'V']
 stars = ['atlas', 'hd23778', 'hd23479', 'alcyone', 'celaeno', 'merope', 'hd23511',  'electra']
+
 
 # get a list of catalog filenames
 cat_fnames = os.listdir(os.path.join(os.getcwd(), 'source_extractor_output'))
@@ -59,7 +68,6 @@ for fname in cat_fnames:
 
 
 
-# TODO: use large aperture files to get magnitudes for the very bright/large stars, and make sure those are the ONLY values used for those stars
 
 data = {} 
 # loop through each star/frame
@@ -68,25 +76,22 @@ for star in stars:
 	for filt in filters:
 		# loop through each catalog file for that star and filter
 		for cat_fname in cat_files[star][filt]:
-			if (star in ['atlas', 'celaeno', 'alcyone', 'merope']) and ('0.1s' in cat_fname):
-				# these exposures are no good (at least for now); source extractor is just detecting noise
-				# TODO: see if some of the exposures are still useable
-				continue
-			exp_time = cat_fname.split('_')[2] # keep track of the exposure time for the image used to produce catalog file
-			# we need to make sure we only take measurements for v and b mag at same exp time
+			if (cat_fname=='atlas_V_0.1s_01.cat') or (cat_fname=='atlas_V_0.1s_03.cat'):# or ('large' in cat_fname):
+				continue # something wrong with these specific files
 
 			# load data from cat file
 			ras, decs, fluxs, flux_errs, mags, mag_errs, background, max_flux = np.loadtxt('source_extractor_output/'+cat_fname, unpack=True)
+			print "loading {} ... {} entries in catalog".format(cat_fname,len(ras))
+
 			for i,ra in enumerate(ras):
 				coords_i = (ras[i], decs[i]) # get coordinates at position i in catalog
-				mag_i = mags[i] + offset[filt] - ext[filt]
+				mag_i = mags[i] + offset[filt] - ext[filt] # get the magnitude and correct it
 				err_i = np.sqrt(mag_errs[i]**2 + offset_err[filt]**2)
 
 				# check if the star is saturated; if so, ignore it, and if not, add its magnitude to the list
 				if (max_flux[i]+background[i] > fmax) or (mag_i >= 95.) or (err_i >= 95.):
 					pass
 				else:
-					#print cat_fname, ras[i], decs[i], mag_i, err_i
 					# check if we already have these coordinates
 					coords = np.array(data.keys()) # list of coordinates we already have
 					if len(coords) > 0: # if this is False, the the data dictionary is empty, and we need to make it
@@ -97,83 +102,80 @@ for star in stars:
 							continue
 						min_dist = dists[loc] # minimum distance
 						guess_coords = coords[loc] # guess of the coordinates 
-						#print min_dist[0], guess_coords[0][0], guess_coords[0][1]	
-						if min_dist[0] < 0.001:
-							data[(guess_coords[0][0], guess_coords[0][1])][filt]['mag'].append(mag_i) # add the magnitude and correct it
+						if min_dist[0] < 0.01: # if it's within 30 arcseconds of a coordinate we've already found
+							data[(guess_coords[0][0], guess_coords[0][1])][filt]['mag'].append(mag_i) # add the magnitude 
 							data[(guess_coords[0][0], guess_coords[0][1])][filt]['err'].append(err_i)
-							data[(guess_coords[0][0], guess_coords[0][1])][filt]['exp_time'].append(exp_time)
-						else:
-							data[coords_i] = {filt: {'mag': [], 'err': [], 'exp_time': []} for filt in filters}
+						else:   # if we don't have this star yet, begin a list of its measureed magnitudes
+							data[coords_i] = {filt: {'mag': [], 'err': []} for filt in filters}
 							data[coords_i][filt]['mag'].append(mag_i)
 							data[coords_i][filt]['err'].append(err_i)
-							data[coords_i][filt]['exp_time'].append(exp_time)
-					else: # if we don't have this star yet, begin a list of its measureed magnitudes
-						data[coords_i] = {filt: {'mag': [], 'err': [], 'exp_time': []} for filt in filters}
+					else: # begin the list of coordinates
+						data[coords_i] = {filt: {'mag': [], 'err': []} for filt in filters}
 						data[coords_i][filt]['mag'].append(mag_i)
 						data[coords_i][filt]['err'].append(err_i)
-						data[coords_i][filt]['exp_time'].append(exp_time)
 
 
 coords = data.keys()
-mags = {}
+
+max_err = 1.  # maximum error for magnitude to include it as a data point
+
+Bmags = []
+Berrs = []
+Vmags = []
+Verrs = []
+BV = []
+BV_err = []
+ra_vals = []
+dec_vals = []
+
 for coord in coords:
 	# need to match up mags obtained from diff exp times - we can't use data for one filter at a given exposure time if we don't have the data for the other filter
 	
 	if (len(data[coord]['V']['mag']) > 0) and (len(data[coord]['B']['mag']) > 0):
 		# temporary lists to hold magnitudes for the star
-		tmpB = []
-		tmpB_errs = []
-		tmpV = []
-		tmpV_errs = []
-		# make sure we have at least one V and B magnitude for each exp time
-		exp_times = set(data[coord]['B']['exp_time'] + data[coord]['V']['exp_time'])
-		for exp_time in exp_times:
-			if (exp_time in data[coord]['B']['exp_time']) and (exp_time in data[coord]['V']['exp_time']):
-				# this means we have data for B and V using that exposure time
-				idxsB = np.where(np.array(data[coord]['B']['exp_time']) == exp_time)
-				idxsV = np.where(np.array(data[coord]['V']['exp_time']) == exp_time)
-				magsB = np.array(data[coord]['B']['mag'])[idxsB]
-				magsV = np.array(data[coord]['V']['mag'])[idxsV]
-				errsB = np.array(data[coord]['B']['err'])[idxsB]
-				errsV = np.array(data[coord]['V']['err'])[idxsV]
-				for i in range(len(magsB)):
-					tmpB.append(magsB[i])
-					tmpB_errs.append(errsB[i])
-				for i in range(len(magsV)):
-					tmpV.append(magsV[i])
-					tmpV_errs.append(errsV[i])
-		if (len(tmpB) > 0) and (len(tmpV)>0):
-			mags[coord] = {filt: {} for filt in filters}
-			wtsB = 1. / np.array(tmpB_errs)**2
-			wtsV = 1. / np.array(tmpV_errs)**2
-			if len(tmpB) > 1:
-				mags[coord]['B']['mag'] = np.average(np.array(tmpB), weights=wtsB)
-				mags[coord]['B']['err'] = np.sqrt(1. / np.sum(wtsB))
-			else:
-				mags[coord]['B']['mag'] = tmpB[0]
-				mags[coord]['B']['err'] = tmpB_errs[0]
-			if len(tmpV) > 1:
-				mags[coord]['V']['mag'] = np.average(np.array(tmpV), weights=wtsV)
-				mags[coord]['V']['err'] = np.sqrt(1. / np.sum(wtsV))
-			else:
-				mags[coord]['V']['mag'] = tmpV[0]
-				mags[coord]['V']['err'] = tmpV_errs[0]
+		tmpB = data[coord]['B']['mag'][:]
+		tmpB_errs = data[coord]['B']['err'][:]
+		tmpV = data[coord]['V']['mag'][:]
+		tmpV_errs = data[coord]['V']['err'][:]
+		mags[coord] = {filt: {} for filt in filters}
+		wtsB = 1. / np.array(tmpB_errs)**2
+		wtsV = 1. / np.array(tmpV_errs)**2
+		mag_B = None
+		mag_V = None
+		# only save the data point if the errors aren't huge (this was checked manually- only very dim stars will be removed)
+		if len(tmpB) > 1:
+			mag_B = np.average(np.array(tmpB), weights=wtsB)
+			mag_B_err = np.sqrt(1. / np.sum(wtsB))
+		else:
+			mag_B = tmpB[0]
+			mag_B_err = tmpB_errs[0]
+		if len(tmpV) > 1:
+			mag_V = np.average(np.array(tmpV), weights=wtsV)
+			mag_V_err = np.sqrt(1. / np.sum(wtsV))
+		else:
+			mag_V = tmpV[0]
+			mag_V_err = tmpV_errs[0]
+		if (mag_B_err <= max_err) and (mag_V_err <= max_err):
+			Bmags.append(mag_B)
+			Berrs.append(mag_B_err)
+			Vmags.append(mag_V)
+			Verrs.append(mag_V_err)
+			BV.append(mag_B - mag_V)
+			BV_err.append(np.sqrt(mag_B_err**2 + mag_V_err**2))
+			ra_vals.append(coord[0])
+			dec_vals.append(coord[1])
 
 
 # save the magnitudes in a text file
-Bmags = []
-Vmags = []
-ra_vals = []
-dec_vals = []
-for coord in mags.keys():
-	Bmags.append(mags[coord]['B']['mag'])
-	Vmags.append(mags[coord]['V']['mag'])
-	ra_vals.append(coord[0])
-	dec_vals.append(coord[1])
 B = np.array(Bmags)
+Berrs = np.array(Berrs)
 V = np.array(Vmags)
-np.savetxt('cat.txt', np.column_stack((ra_vals, dec_vals, B, V)), header="ra dec B V")
+Verrs = np.array(Verrs)
+BV = np.array(BV)
+BV_err = np.array(BV_err)
+np.savetxt('cat_v7.txt', np.column_stack((ra_vals, dec_vals, B, Berrs, V, Verrs, BV, BV_err)), header="ra, dec, B, B err, V, V err, B-V, B-V err")
 
+print "{} points out of {} detected stars".format(len(V), len(data.keys()))
 
 # plots for testing:
 
@@ -182,20 +184,28 @@ ra, dec = zip(*data.keys())
 plt.figure(figsize=(10,10))
 plt.plot(ra, dec, 'x')
 plt.grid()
-plt.savefig('test.png')
+plt.savefig('test_v4.png')
 
 # plot every point we're using
 ra, dec = zip(*mags.keys())
 plt.figure(figsize=(10,10))
 plt.plot(ra, dec, 'x')
 plt.grid()
-plt.savefig('test2.png')
+plt.savefig('test2_v4.png')
 
 # CMD
-print "plot: {} points".format(len(mags.keys()))
 plt.figure(figsize=(10,10))
 plt.plot(B-V, V, 'o')
 plt.grid()
+plt.gca().invert_yaxis()
 plt.xlabel("B-V")
 plt.ylabel("V")
-plt.savefig('cmd.png')
+plt.savefig('cmd_v4.png')
+
+plt.figure(figsize=(10,10))
+plt.errorbar(BV, V, xerr=BV_err, yerr=Verrs, fmt='.', lw=0.9, capsize=2, alpha=0.85)
+plt.grid()
+plt.gca().invert_yaxis()
+plt.xlabel("B-V")
+plt.ylabel("V")
+plt.savefig('cmd_err_v4.png')
